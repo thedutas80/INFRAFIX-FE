997dce1b2e3d644b745bb27b457d5c57a1dfc624
Muchammad Muchib Zainul Fikry <60887634+sourchib@users.noreply.github.com>
2025-12-08 23:08:01 +0800

    public
    cloud

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// export const API_BASE_URL = 'http://103.164.191.212:8082'
-export const API_BASE_URL = 'http://localhost:8080'
+export const API_BASE_URL = 'http://103.164.191.212:8082'
+//export const API_BASE_URL = 'http://localhost:8080'
 export const api = axios.create({
   baseURL: API_BASE_URL
 })
